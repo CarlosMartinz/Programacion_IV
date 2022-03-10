0567a96874f88e860c777fe8b4b9f29f18d8ef0b
CarlosMartinz <68661277+CarlosMartinz@users.noreply.github.com>
2022-03-09 19:35:18 -0600

    Creacion de la rama primer_parcial

--- docs/main.js
+++ docs/main.js
@@ -1,51 +1,68 @@
-var generarIdUnicoFecha = ()=>{
+var idUnicoFecha = ()=>{
     let fecha = new Date();
     return Math.floor(fecha.getTime()/1000).toString(16);
 }, db;
-var appSistema = new Vue({
+var app = new Vue({
     el: '#appSistema',
     data: {
-        forms:{
-            'cliente':{mostrar:false}
-        }
+       forms:{
+           'cliente':{ mostrar: false },
+           'categoria':{ mostrar: false },
+           'producto':{ mostrar: false },
+           'proveedor':{ mostrar: false },
+       }
     },
     methods: {
         abrirBd(){
             /**
-             * Mecanismos de Almacenamiento
+             * Mecanismo de almacenamiento local:
              * 1. WebSQL
              * 2. localStorage
              * 3. IndexedDB
              */
-            let indexDb = indexedDB.open('db_sistema', 1);
-            indexDb.onupgradeneeded = e=>{
-                let db = e.target.result;
-                tblcliente = db.createObjectStore('cliente', {keyPath:'idCliente'});
+            let indexDb = indexedDB.open('db_sistema',1);
+            indexDb.onupgradeneeded = (e)=>{
+                let db = e.target.result,
+                tblclientes = db.createObjectStore('clientes',{ keyPath: 'idCliente' }),
+                tblcategorias = db.createObjectStore('categorias',{ keyPath: 'idCategoria' }),
+                tblproductos = db.createObjectStore('productos',{ keyPath: 'idProducto' }),
+                tblproveedores = db.createObjectStore('proveedores',{ keyPath: 'idProveedor' });
 
-                tblcliente.createIndex('idCliente', 'idCliente', {unique:true});
-                tblcliente.createIndex('codigo', 'codigo', {unique:false});
+                tblproductos.createIndex('idProducto','idProducto',{ unique: true });
+                tblproductos.createIndex('codigo','codigo',{ unique: false });
+                tblproductos.createIndex('id','id',{ unique: false });
+
+                tblcategorias.createIndex('idCategroia','idCategoria', { unique: true });
+                tblcategorias.createIndex('codigo','codigo', { unique: false });
+
+                tblclientes.createIndex('idCliente','idCliente', { unique: true });
+                tblclientes.createIndex('codigo', 'codigo',{unique:false});
+
+                tblproveedores.createIndex('idProdveedor','idProveedor', { unique: true });
+                tblproveedores.createIndex('codigo','codigo', { unique: false });  
             };
-            indexDb.onsuccess = e=>{
+            indexDb.onsuccess = (e)=>{
                 db = e.target.result;
             };
-            indexDb.onerror = e=>{
+            indexDb.onerror = (e)=>{
                 console.log(e.target.error);
             };
-        },
+        }
     },
     created(){
-        this.abrirBd();
+       this.abrirBd();
     }
 });
-function abrirStore(store, modo){
-    return db.transaction(store, modo).objectStore(store);
-}
-document.addEventListener('DOMContentLoaded', e=>{
-    let formularios = document.querySelectorAll('.mostrar').forEach(formulario=>{
-        formulario.addEventListener('click', evento=>{
-            let formulario = evento.target.dataset.form;
-            appSistema.forms[formulario].mostrar = true;
-            appSistema.$refs[formulario].obtenerDatos();
+document.addEventListener('DOMContentLoaded', event=>{
+    let $element = document.querySelectorAll('.mostrar').forEach( (element,index)=>{
+        element.addEventListener('click', e=>{
+            app.forms[e.target.dataset.form].mostrar = true;
+            app.$refs[e.target.dataset.form].obtenerDatos();
         });
     });
 });
+
+function abrirStore(store, modo){
+    let tx = db.transaction(store, modo);
+    return tx.objectStore(store);
+}
